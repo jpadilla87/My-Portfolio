64eee0d53f65b54e828bb6f04bfd3458b19012da
Jorell Padilla <jorellpadilla@gmail.com>
2021-11-26 21:05:57 -0600

    commit

--- pages/Chatbot/scripts/chat.js
+++ pages/Chatbot/scripts/chat.js
@@ -1,19 +1,19 @@
-// COllapsible
-let coll = document.getElementsByClassName("collapsible");
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+    coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
 
-        let content = this.nextElementSibling;
+        var content = this.nextElementSibling;
 
         if (content.style.maxHeight) {
             content.style.maxHeight = null;
         } else {
             content.style.maxHeight = content.scrollHeight + "px";
         }
-    });
 
+    });
 }
 
 function getTime() {
@@ -22,48 +22,47 @@ function getTime() {
     minutes = today.getMinutes();
     ampm = hours >= 12 ? 'pm' : 'am';
 
-    if (hours > 12) {
-        hours -= 12;
-    } else if (hours === 0) {
-       hours = 12;
-    }
-
+    if (hours > 12) { hours -= 12; } else if (hours === 0) { hours = 12;}
+    
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
 
-    let time = hours + ":" + minutes + ' ' + ampm;
+    let time = hours + ":" + minutes + ampm;
     return time;
 }
 
+// Gets the first message
 function firstBotMessage() {
     let firstMessage = "Hello! I am a bot designed to tell you the compatibility of red blood cells. Type the blood type you are interested in finding compatibility information about."
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
-    let time = getTime;
+    let time = getTime();
 
     $("#chat-timestamp").append(time);
-    document.getElementById("userInput").scrollIntroView(false);
+    document.getElementById("userInput").scrollIntoView(false);
 }
 
 firstBotMessage();
 
+// Retrieves the response
 function getHardResponse(userText) {
     let botResponse = getBotResponse(userText);
-    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>'
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
     $("#chatbox").append(botHtml);
 
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
+//Gets the text text from the input box and processes it
 function getResponse() {
     let userText = $("#textInput").val();
 
     if (userText == "") {
-        userText = "I forgot to type a valid response."
+        userText = "I forgot to type a valid response.";
     }
 
-    let userHtml = '<p class="userText"><span' + userText + '</span></p>';
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
 
     $("#textInput").val("");
     $("#chatbox").append(userHtml);
@@ -71,12 +70,14 @@ function getResponse() {
 
     setTimeout(() => {
         getHardResponse(userText);
-    }, 500)
+    }, 1000)
 
 }
 
+// Handles sending text via button clicks
 function buttonSendText(sampleText) {
-    let userhtml = '<p class="userText"><span' + sampleText + '</span></p>';
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+
     $("#textInput").val("");
     $("#chatbox").append(userHtml);
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
@@ -90,8 +91,9 @@ function heartButton() {
     buttonSendText("Thank you!")
 }
 
-$("#textInput").keypress(function(e) {
-    if(e.key == 13) {
+// Press enter to send a message
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
         getResponse();
     }
 });
